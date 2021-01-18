88e5e68718df93654e85791c0109a98c0800495d
Maicon Ribeiro Esteves <maicon.burn@gmail.com>
2021-01-18 10:40:49 +0000

    Toggle button

--- components/Projects.js
+++ components/Projects.js
@@ -10,10 +10,33 @@ import Grid from '@material-ui/core/Grid'
 import Box from '@material-ui/core/Box'
 import Button from '@material-ui/core/Button'
 import ButtonGroup from '@material-ui/core/ButtonGroup'
+import ToggleButton from '@material-ui/lab/ToggleButton'
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup'
 import styles from '../styles/Projects.module.scss'
 
 
 export default function Projects() {
+    const [purchasingFor, setPurchasingFor] = React.useState('business')
+    const [purchasingBefore, setPurchasingBefore] = React.useState('no')
+    const [factorImportant, setFactorImportant] = React.useState('budget')
+    const [projectPlace, setProjectPlace] = React.useState('global')
+    const [projectType, setProjectType] = React.useState('social')
+
+    const handlePurchasingFor = (event, newPurchasingFor) => {
+        setPurchasingFor(newPurchasingFor)
+    }
+    const handlePurchasingBefore = (event, newPurchasingBefore) => {
+        setPurchasingBefore(newPurchasingBefore)
+    }
+    const handleFactorImportant = (event, newFactorImportant) => {
+        setFactorImportant(newFactorImportant)
+    }
+    const handleProjectPlace = (event, newProjectPlace) => {
+        setProjectPlace(newProjectPlace)
+    }
+    const handleProjectType = (event, newProjectType) => {
+        setProjectType(newProjectType)
+    }
     return (
         <Container maxWidth="lg" className={styles.projects}>
             <Box xs='12'>
@@ -32,10 +55,16 @@ export default function Projects() {
                                     </Typography>
                                 </Grid>
                                 <Grid item xs='12'>
-                                    <ButtonGroup color="secondary" aria-label="large outlined primary button group">
-                                        <Button variant="contained">I’m purchasing for a business</Button>
-                                        <Button>I’m purchasing as an individual</Button>
-                                    </ButtonGroup>
+                                    <ToggleButtonGroup 
+                                        value={purchasingFor}
+                                        exclusive
+                                        onChange={handlePurchasingFor}
+                                        aria-label="Who are you purchasing for?"
+                                        color="secondary" 
+                                    >
+                                        <ToggleButton value="business" >I’m purchasing for a business</ToggleButton>
+                                        <ToggleButton value="individual" >I’m purchasing as an individual</ToggleButton>
+                                    </ToggleButtonGroup>
                                 </Grid>
                                 <Grid item xs='12'>
                                     <Typography gutterBottom variant="h6" component="h6">
@@ -43,10 +72,16 @@ export default function Projects() {
                                     </Typography>
                                 </Grid>
                                 <Grid item xs='12'>
-                                    <ButtonGroup color="secondary" aria-label="large outlined primary button group">
-                                        <Button>Yes</Button>
-                                        <Button variant="contained">No</Button>
-                                    </ButtonGroup>
+                                    <ToggleButtonGroup 
+                                        value={purchasingBefore}
+                                        exclusive
+                                        onChange={handlePurchasingBefore}
+                                        aria-label="Have you purchased carbon credits before?"
+                                        color="secondary" 
+                                    >
+                                        <ToggleButton value="yes" >Yes</ToggleButton>
+                                        <ToggleButton value="no" >No</ToggleButton>
+                                    </ToggleButtonGroup>
                                 </Grid>
                                 <Grid item xs='12'>
                                     <Typography gutterBottom variant="h6" component="h6">
@@ -54,10 +89,16 @@ export default function Projects() {
                                     </Typography>
                                 </Grid>
                                 <Grid item xs='12'>
-                                    <ButtonGroup color="secondary" aria-label="large outlined primary button group">
-                                        <Button>Budget ($6–$18 per credit)</Button>
-                                        <Button variant="contained">Environmental Impact ($18–$50 per credit)</Button>
-                                    </ButtonGroup>
+                                    <ToggleButtonGroup 
+                                        value={factorImportant}
+                                        exclusive
+                                        onChange={handleFactorImportant}
+                                        aria-label="Which factor is more important to you?"
+                                        color="secondary" 
+                                    >
+                                        <ToggleButton value="budget" >Budget ($6–$18 per credit)</ToggleButton>
+                                        <ToggleButton value="impact" >Environmental Impact ($18–$50 per credit)</ToggleButton>
+                                    </ToggleButtonGroup>
                                 </Grid>
                                 <Grid item xs='12'>
                                     <Typography gutterBottom variant="h6" component="h6">
@@ -65,23 +106,35 @@ export default function Projects() {
                                     </Typography>
                                 </Grid>
                                 <Grid item xs='12'>
-                                    <ButtonGroup color="secondary" aria-label="large outlined primary button group">
-                                        <Button>Local Projects</Button>
-                                        <Button variant="contained">Global Projects</Button>
-                                        <Button>Both</Button>
-                                    </ButtonGroup>
+                                    <ToggleButtonGroup 
+                                        value={projectPlace}
+                                        exclusive
+                                        onChange={handleProjectPlace}
+                                        aria-label=" Would you like to support local or global projects?"
+                                        color="secondary" 
+                                    >
+                                        <ToggleButton value="local" >Local Projects</ToggleButton>
+                                        <ToggleButton value="global" >Global Projects</ToggleButton>
+                                        <ToggleButton value="both" >Both</ToggleButton>
+                                    </ToggleButtonGroup>
                                 </Grid>
                                 <Grid item xs='12'>
                                     <Typography gutterBottom variant="h6" component="h6">
-                                        Would you like to support local or global projects?
+                                        What kind of projects do you want to support?
                                     </Typography>
                                 </Grid>
                                 <Grid item xs='12'>
-                                    <ButtonGroup size="small" color="secondary" aria-label="large outlined primary button group">
-                                        <Button>Reforestation</Button>
-                                        <Button variant="contained">Renewable Energy</Button>
-                                        <Button>Social Projects</Button>
-                                    </ButtonGroup>
+                                    <ToggleButtonGroup 
+                                        value={projectType}
+                                        exclusive
+                                        onChange={handleProjectType}
+                                        aria-label="What kind of projects do you want to support?"
+                                        color="secondary" 
+                                    >
+                                        <ToggleButton value="Reforestation" >Reforestation</ToggleButton>
+                                        <ToggleButton value="energy" >Renewable Energy</ToggleButton>
+                                        <ToggleButton value="social" >Social Projects</ToggleButton>
+                                    </ToggleButtonGroup>
                                 </Grid>
                             </Grid>
                         </Card> 
