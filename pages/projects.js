270fefd6ca3490c24c00b915cf66b773d2a8d389
Maicon Ribeiro Esteves <maicon.burn@gmail.com>
2021-01-23 17:03:47 +0000

    Teste

--- pages/projects.js
+++ pages/projects.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import Projects from '../components/Projects'
+import ProjectsList from '../components/ProjectsList'
 
 export default function Index() {
   return (
     <>
       <NavBar />
       <Projects />
+      <ProjectsList />
     </>
   )
 }
